4edb88529f48315275dd1fc977e855e9b0c5193c
6x68mx <6x68mx@gmail.com>
2018-02-03 19:47:14 +0100

    initial commit

---
+++ apolloapi.py
@@ -0,0 +1,203 @@
+import mechanicalsoup
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import json
+
+SITE_URL = "https://apollo.rip"
+
+USER_AGENT = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3)"
+              "AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.79"
+              "Safari/535.11")
+
+FORMATS = {
+    "flac": {
+        "format": "FLAC",
+        "bitrate": "Lossless"
+    },
+    "320": {
+        "format": "MP3",
+        "bitrate": "320"
+    },
+    "v0": {
+        "format": "MP3",
+        "bitrate": "V0 (VBR)"
+    },
+    "v2": {
+        "format": "MP3",
+        "bitrate": "V2 (VBR)"
+    }
+}
+
+class ApolloApi:
+    def __init__(self, username=None, password=None):
+        self.browser = mechanicalsoup.StatefulBrowser(
+                soup_config={"features": "lxml"},
+                raise_on_404=True,
+                user_agent=USER_AGENT
+        )
+        self.session = requests.Session()
+        self.session.headers.update({"User-Agent": USER_AGENT})
+        self.authenticated = False
+        self.rate_limit = 2
+        self.last_request = time.time()
+
+    def login(self, username, password):
+        r = self.session.post(SITE_URL + "/login.php",
+                              data={"username": username,
+                                    "password": password,
+                                    "login": "Log in"},
+                              allow_redirects=False)
+        if r.status_code == 302 and r.headers["location"] != "login.php":
+            r = self.get_index()
+            if r is not None:
+                self.username = r["username"]
+                self.uid = r["id"]
+                self.authkey = r["authkey"]
+                self.passkey = r["passkey"]
+                self.authenticated = True
+                return True
+
+        return False
+
+    def _api_request(self, action, **kwargs):
+        while time.time() - self.last_request < self.rate_limit:
+            time.sleep(0.1)
+        self.last_request = time.time()
+
+        params = {"action": action}
+        params.update(kwargs)
+        r = self.session.get(SITE_URL + "/ajax.php", params=params)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return None
+
+    def get_better_snatched(self):
+        if not self.authenticated:
+            return
+
+        re_artist = re.compile(r"artist\.php\?id=(?P<artistid>[0-9]+)")
+        re_torrent = re.compile(r"torrents\.php\?id=(?P<groupid>[0-9]+)&torrentid=(?P<torrentid>[0-9]+)")
+
+        r = self.session.get(SITE_URL + "/better.php?method=snatch")
+
+        if r.status_code != 200:
+            return
+
+        soup = BeautifulSoup(r.content, "lxml")
+
+        torrents = []
+        for entry in soup.find_all("tr", class_="torrent_row"):
+            t = {}
+
+            artist = entry.find("a", href=re_artist)
+            if artist:
+                t["artist"] = artist.string
+                t["artistid"] = re_artist.search(artist.get("href"))["artistid"]
+            else:
+                t["artist"] = "Various Artists"
+
+            torrent = entry.find("a", href=re_torrent)
+            t["name"] = torrent.string
+            r = re_torrent.search(torrent.get("href"))
+            t["groupid"] = r["groupid"]
+            t["torrentid"] = r["torrentid"]
+
+            needed = []
+            v2 = entry.td.find_next_sibling("td")
+            if v2.string == "NO":
+                needed.append("v2")
+            v0 = v2.find_next_sibling("td")
+            if v0.string == "NO":
+                needed.append("v0")
+            f320 = v0.find_next_sibling("td")
+            if f320.string == "NO":
+                needed.append("320")
+            t["formats_needed"] = needed
+
+            torrents.append(t)
+
+        return torrents
+
+    def get_torrent(self, tid):
+        return self._api_request("torrent", id=tid)
+
+    def get_index(self):
+        r = self._api_request("index")
+        if r is not None and r.get("status", "") == "success":
+            return r["response"]
+        else:
+            return None
+    
+    def add_format(self, torrent, format, tfile, description=""):
+        if format not in FORMATS:
+            return False # TODO indicate "not a valid format" error
+
+        gid = torrent["group"]["id"]
+        torrent = torrent["torrent"]
+
+        data = {
+            "submit": "true",
+            "auth": self.authkey,
+            "groupid": str(gid),
+            "type": 0,
+            "format": FORMATS[format]["format"],
+            "bitrate": FORMATS[format]["bitrate"],
+            "media": torrent["media"],
+            "release_desc": description
+        }
+
+        if torrent["remastered"]:
+            data["remaster"] = "on"
+            data["remaster_year"] = str(torrent["remasterYear"])
+            data["remaster_title"] = torrent["remasterTitle"]
+            data["remaster_record_label"] = torrent["remasterRecordLabel"]
+            data["remaster_catalogue_number"] = torrent["remasterCatalogueNumber"]
+            
+
+        files = {"file_input": (tfile.name, tfile.open("rb"), "application/x-bittorrent")}
+
+        r = self.session.post(SITE_URL + "/upload.php",
+                              params={"groupid": gid},
+                              data=data,
+                              files=files,
+                              allow_redirects=False)
+
+        if r.status_code == 302:
+            return True
+        else:
+            return False
+
+class TorrentCache:
+    def __init__(self, api, path=None):
+        self.api = api
+        self.clear()
+        if path:
+            self.load(path)
+        
+    def clear(self):
+        self.torrents = {}
+
+    def load(self, path):
+        try:
+            with open(path, "r") as f:
+                self.torrents.update(json.load(f))
+        except FileNotFoundError:
+            pass
+
+    def save(self, path):
+        with open(path, "w") as f:
+            json.dump(self.torrents, f)
+
+    def get(self, tid, caching=True):
+        if tid in self.torrents:
+            return self.torrents[tid]
+        else:
+            t = self.api.get_torrent(tid)
+            if t and "status" in t and t["status"] == "success":
+                self.torrents.update({tid: t})
+                return t
+            else:
+                return None
---
+++ apollobetter.py
@@ -0,0 +1,129 @@
+from apolloapi import ApolloApi, TorrentCache
+import argparse
+import configparser
+from pathlib import Path
+import re
+import subprocess
+
+CONFIG_PATH = "apollobetter.conf"
+ANNOUNCE_URL = "https://mars.apollo.rip/{}/announce"
+
+def get_transcode_dir(flac_dir, output_format):
+    output_format = output_format.upper()
+    if 'FLAC' in flac_dir.upper():
+        transcode_dir = re.sub(re.compile('FLAC', re.I), output_format, flac_dir)
+    else:
+        transcode_dir = flac_dir + " (" + output_format + ")"
+        if output_format != 'FLAC':
+            transcode_dir = re.sub(re.compile('FLAC', re.I), '', transcode_dir)
+    return transcode_dir
+
+def create_torrent_file(torrent_path, data_path, tracker, passkey, piece_length):
+    if torrent_path.exists() or not data_path.exists():
+        return False
+    
+    url = tracker.format(passkey)
+    command = ["mktorrent", "-p", "-l", str(piece_length), "-a", url, "-s", "APL", "-o", torrent_path, data_path]
+    subprocess.check_output(command, stderr=subprocess.STDOUT)
+
+def main():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--search-dir", help="Where to search for potential uploads", type=Path, required=True)
+    parser.add_argument("-o", "--output-dir", help="Destination for converted data", type=Path, required=True)
+    parser.add_argument("--torrent-dir", help="Where to put the new *.torrent files", type=Path, required=True)
+    #parser.add_argument("--torrent-search-dir", help="Where to search for torrent files", type=Path, required=True)
+    parser.add_argument("-l", "--limit", type=int, help="Maximum number of torrents to upload", default=0)
+    parser.add_argument("-v2", "--format-v2", action="store_true")
+    parser.add_argument("-v0", "--format-v0", action="store_true")
+    parser.add_argument("-320", "--format-320", action="store_true")
+    args = parser.parse_args()
+
+    formats = []
+    if args.format_v2:
+        formats.append("v2")
+    if args.format_v0:
+        formats.append("v0")
+    if args.format_320:
+        formats.append("320")
+    if formats:
+        formats = set(formats)
+    else:
+        formats = {"v2", "v0", "320"}
+
+    print("Logging in...")
+    username = config["apollo"]["username"]
+    password = config["apollo"]["password"]
+    api = ApolloApi()
+    if not api.login(username, password):
+        print("Error: Can't login to apollo.rip")
+        return
+
+    cache_path = config["DEFAULT"]["torrent_cache"]
+    tc = TorrentCache(api, cache_path)
+
+    print("Fetching potential upload candidates from apollo...")
+    candidates = api.get_better_snatched()
+
+    candidates = [c for c in candidates if any(f in c["formats_needed"] for f in formats)]
+
+    if not candidates:
+        print("Their are no candidates for conversion. Nothing to do, exiting...")
+    else:
+        print("Found {} potential candidates.".format(len(candidates)))
+
+    nuploaded = 0
+    limit = args.limit
+    for c in candidates:
+        if limit > 0 and nuploaded >= limit:
+            break
+
+        print("Processing {} - {} (ID: {}), Needed: {}".format(c["artist"], c["name"], c["torrentid"], ", ".join(c["formats_needed"])))
+        
+        torrent_response = tc.get(c["torrentid"])
+        torrent = torrent_response["response"]["torrent"]
+
+        needed = formats.intersection(c["formats_needed"])
+        for output_format in needed:
+
+            # find the torrent data
+            transcode_dir = get_transcode_dir(torrent["filePath"], output_format)
+            path = args.search_dir / transcode_dir
+            if not path.exists():
+                print("\tFiles for \"{}\" not found. Continuing with next Candidate...".format(output_format))
+                continue
+            print("\tFound {}.".format(path))
+            
+            # TODO check integrity i.e. if file list in from torrent matches the actuall files on disk
+
+            # find the torrent file
+            """
+            tfile = args.torrent_search_dir / (transcode_dir + ".torrent")
+            if not tfile.exists():
+                print("\t\tCouldn't find corresponding torrent file.")
+                continue
+            print("\t\tFound {}.".format(tfile))
+            """
+
+            print("\t\tCreating torrent file...")
+            tfile = args.torrent_dir / (transcode_dir + ".torrent")
+            create_torrent_file(tfile, path, ANNOUNCE_URL, api.passkey, 18)
+
+            print("\t\tUploading torrent...")
+            r = api.add_format(torrent_response["response"], output_format, tfile)
+            if r:
+                print("Done.")
+            else:
+                print("Error on upload. Aborting everything!")
+
+            nuploaded += 1
+
+    print("\nFinished")
+    print("Uploaded {} torrents.".format(nuploaded))
+
+    tc.save(cache_path)
+
+if __name__ == "__main__":
+    main()
