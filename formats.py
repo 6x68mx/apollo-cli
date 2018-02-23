624f54e20a0cc957b1c0a13ee908efd14d3a2a0e
6x68mx <6x68mx@gmail.com>
2018-02-23 16:52:40 +0100

    added a NAME attribute to the fromat classes and made FORMATS a set

--- formats.py
+++ formats.py
@@ -1,5 +1,6 @@
 
 class FormatFlac():
+    NAME = "FLAC"
     FORMAT = "FLAC"
     BITRATE = "Lossless"
     FILE_EXT = "flac"
@@ -11,6 +12,7 @@ def lame_cmd(dst, opts):
     return ["lame", "-S", *opts, "-", dst]
 
 class Format320:
+    NAME = "320"
     FORMAT = "MP3"
     BITRATE = "320"
     FILE_EXT = "mp3"
@@ -19,6 +21,7 @@ class Format320:
         return lame_cmd(dst, ["-h", "-b", "320", "--ignore-tag-errors"])
 
 class FormatV0:
+    NAME = "V0"
     FORMAT = "MP3"
     BITRATE = "V0 (VBR)"
     FILE_EXT = "mp3"
@@ -27,6 +30,7 @@ class FormatV0:
         return lame_cmd(dst, ["-V", "0", "--vbr-new", "--ignore-tag-errors"])
 
 class FormatV2:
+    NAME = "V2"
     FORMAT = "MP3"
     BITRATE = "V2 (VBR)"
     FILE_EXT = "mp3"
@@ -34,9 +38,9 @@ class FormatV2:
     def encode_cmd(dst):
         return lame_cmd(dst, ["-V", "2", "--vbr-new", "--ignore-tag-errors"])
 
-FORMATS = [
+FORMATS = (
         FormatFlac,
         Format320,
         FormatV0,
         FormatV2
-]
+)
